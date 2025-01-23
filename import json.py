89942e0cdf55b9df61295a34ad4fc01dd7a7f65d
ElvisOC <de.elvis@laposte.net>
2025-01-23 16:44:33 +0100

    modele player

--- import json.py
+++ import json.py
@@ -1,6 +1,11 @@
 import json
 import os
+import pandas as pd
+import pathlib
+from operator import itemgetter
+from tabulate import tabulate
 
+parent_directory = pathlib.Path(__file__).parent.resolve()
 directory = os.path.expandvars(fr'C:\Users\%username%\Desktop\openclassrooms\P4\data\player')
 file_path = os.path.join(directory, 'players.json')
 
@@ -8,7 +13,8 @@ file_path = os.path.join(directory, 'players.json')
 
 
 class Player:
-    def __init__(self, name, surname, birthdate, file_path):
+    def __init__(self, national_id, name, surname, birthdate, file_path):
+        self.national_id = national_id
         self.name = name
         self.surname = surname
         self.birthdate = birthdate
@@ -20,26 +26,22 @@ class Player:
                 try: 
                     return json.load(file)
                 except json.JSONDecodeError:
-                    return {}
+                    return True
         else:
             return {}
         
     def verifie_doublon(self):
         data = self.verifie_file()
         for player in data.values():
-            if (
-                player.get('name') == self.name and
-                player.get('surname') == self.surname and
-                player.get('birthdate') == self.birthdate
-            ):
+            if player.get('ID') == self.national_id:
                 return True
             return False
         
     def addplayer(self):
-        data = self.verifie_file()
+        if self.verifie_file():
+            return "Problème avec le fichier"
         if self.verifie_doublon():
             return f"Le joueur {self.surname} {self.name} existe déjà"
-        
         max_id = max(map(int, data.keys())) if data else 0
         new_player_id = max_id + 1
         data[str(new_player_id)] = {
@@ -50,7 +52,21 @@ class Player:
         with open (file_path, 'w', encoding='utf-8') as file:
             json.dump(data, file, indent=4, ensure_ascii=False)
             return f"Joueur {self.name} {self.surname} ajouté"
+        
+    def afficher_liste_player(self):
+        if not os.path.exists(self.file_path):
+            return "Fichier inexistant"
+        if os.path.getsize(self.file_path) < 0:
+            return "Fichier vide"
+        with open (self.file_path, 'r', encoding='utf-8') as file:
+            try: 
+                data = json.load(file)
+                data_list = [v for v in data.values()]
+                sorted_data = sorted(data_list, key=itemgetter('name'))
+                return tabulate(sorted_data, headers="keys", tablefmt="grid")
+            except json.JSONDecodeError:
+                return "Problème avec le fichier"
             
-new_player = Player(name='test', surname='test', birthdate='31-07-1994', file_path=file_path)
-result = new_player.addplayer()
+new_player = Player(national_id='AC12345', name='test', surname='moi', birthdate='31-07-1994', file_path=file_path)
+result = new_player.afficher_liste_player()
 print(result)
--- test.py
+++ test.py
@@ -1,12 +1,29 @@
 import json
 import os
+import pathlib
+from operator import itemgetter
+from tabulate import tabulate
 
 
 directory = os.path.expandvars(fr'C:\Users\%username%\Desktop\openclassrooms\P4\data\player')
 file_path = os.path.join(directory, 'players.json')
-with open(file_path, 'r') as file:
-    data = json.load(file)
-    
-print (data)
-key = data.keys()
-print (key)
+
+print (pathlib.Path(__file__).parent.resolve())
+
+
+
+def verifie_file(file_path):
+    if os.path.exists(file_path) and os.path.getsize(file_path) > 0:
+        with open (file_path, 'r', encoding='utf-8') as file:
+            try: 
+                data = json.load(file)
+                data_list = [v for v in data.values()]
+                sorted_data = sorted(data_list, key=itemgetter('name'))
+                return tabulate(sorted_data, headers="keys", tablefmt="grid")
+            except json.JSONDecodeError:
+                return True
+    else:
+        return False
+        
+result = verifie_file(file_path)
+print (result)
